a3630f90cdf5db2bec835464cb0e57237c67e7f7
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2022-07-28 15:33:22 +0800

    refactor: update constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -5,12 +5,13 @@ export const IS_PROD = process.env.NODE_ENV === "production";
 // Request
 export const REQUEST_URL_SLUG = "/api/episerver";
 export const REQUEST_ACCEPT_HEADER = "application/json";
-export const REQUEST_CONTENT_TYPE_HEADER = "application/json";
 export const REQUEST_HEADERS = {
-	"Accept": REQUEST_ACCEPT_HEADER,
-	"Content-Type": REQUEST_CONTENT_TYPE_HEADER
+	Accept: REQUEST_ACCEPT_HEADER
 };
 export const REQUEST_TIMEOUT = 0;
+
+// Auth
+export const AUTH_REQUEST_CONTENT_TYPE_HEADER = "application/x-www-form-urlencoded";
 
 // Optimizely
 export const OPTIMIZELY_AUTH_ENDPOINT = "/auth/token";
