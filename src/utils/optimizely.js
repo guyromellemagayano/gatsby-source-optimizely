0cb4b9d56af2f7d8572efa353fad6b2f67e1d1da
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2022-09-01 15:33:42 +0800

    fix: remove console logs

--- src/utils/optimizely.js
+++ src/utils/optimizely.js
@@ -241,8 +241,6 @@ class Optimizely {
 				temp.contentBlocksBottom = expandedContentBlocksBottom;
 			}
 
-			console.log("temp", JSON.stringify(temp, null, 2));
-
 			return temp;
 		} else return data;
 	}
