4ab4332a84fd8695969e8ca6ce248b1b6e526a14
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2022-10-31 23:25:33 +0800

    [WIP] Fix API throttling issues

--- src/utils/optimizely.js
+++ src/utils/optimizely.js
@@ -1,8 +1,10 @@
+/* eslint-disable prettier/prettier */
 "use strict";
 
-import sleep from "sleep-promise";
-import { CONTENT_ENDPOINT } from "../constants";
-import { Request } from "./request";
+import axios from "axios";
+import { REQUEST_ACCEPT_HEADER, REQUEST_MAX_COUNT, REQUEST_URL_SLUG } from "../constants";
+import { convertStringToLowercase, convertStringToUppercase } from "./convertValues";
+import { debounce } from "./debounce";
 
 export class Optimizely {
 	constructor(config) {
@@ -10,248 +12,114 @@ export class Optimizely {
 			throw new Error("Optimizely/Episerver API config required. It is required to make any call to the API");
 		}
 
-		this.site_url = config.site_url;
+		this.site_url = config.site_url + REQUEST_URL_SLUG;
 		this.response_type = config.response_type;
 		this.headers = config.headers;
-		this.log = config.log;
+		this.pending_requests = 0;
 		this.request_timeout = config.request_timeout;
 		this.request_throttle_interval = config.request_throttle_interval;
+		this.request_debounce_interval = config.request_debounce_interval;
 	}
 
-	// Handle API requests
-	async request(method, path, body = null, headers = {}) {
-		// Prepare `path` for request execution
-		const request = new Request(this.site_url, {
+	async request(url = null, nodeName = null, method = null, body = null, headers = null) {
+		const updatedUrl = this.site_url + url;
+
+		const RequestAxiosInstance = axios.create({
+			baseURL: this.site_url,
 			headers: Object.assign({}, this.headers, headers),
-			response_type: this.response_type,
-			log: this.log,
-			request_timeout: this.request_timeout,
-			request_throttle_interval: this.request_throttle_interval
+			responseType: this.response_type,
+			withCredentials: true,
+			timeout: this.request_timeout
 		});
 
-		// Run request
-		const { data } = await request.run(method, path, body);
+		RequestAxiosInstance.defaults.headers.common["Accept"] = REQUEST_ACCEPT_HEADER;
 
-		// Handle expanded keys and values
-		const handleExpandedKeyValues = async (values, label) => {
-			if (values && Array.isArray(values) && values?.length > 0) {
-				let promises = [];
+		RequestAxiosInstance.interceptors.request.use(
+			(config) =>
+				new Promise((resolve) => {
+					// Send request log message to console
+					console.warn(`[${convertStringToUppercase(method)}] ${updatedUrl} - ${nodeName} - (SENT)`);
 
-				values.map(async (value) => {
-					if (value && Object.prototype.hasOwnProperty.call(value, "contentLink") && Object.keys(value)?.length > 0) {
-						if (value.contentLink.id) {
-							promises.push(request.run("get", CONTENT_ENDPOINT + value.contentLink.id + "?expand=*", body));
-						} else {
-							throw new Error("Expanded `contentLink` in `" + label + "` is missing `id` key");
+					// Debounce requests to avoid rate limiting
+					let debounceInterval = debounce(() => this.pending_requests--, this.request_debounce_interval);
+
+					// Throttle requests to avoid rate limiting
+					let throttleInterval = setInterval(() => {
+						if (this.pending_requests < REQUEST_MAX_COUNT) {
+							clearInterval(throttleInterval);
+							debounceInterval();
+							resolve(config);
 						}
-					} else {
-						throw new Error("Expanded `" + label + "` is missing `contentLink` key");
+					}, this.request_throttle_interval);
+
+					// Wait for all throttle to clear
+					throttleInterval;
+
+					// Return null if there are too many pending requests
+					if (this.pending_requests > REQUEST_MAX_COUNT) {
+						console.error(`[${convertStringToUppercase(method)}] ${updatedUrl} - ${nodeName} - (THROTTLED)`);
+						resolve(null);
 					}
-				});
 
-				await Promise.allSettled(promises)
-					.then((res) => {
-						if (res && Array.isArray(res) && res?.length > 0) {
-							res
-								.filter(({ status = null, value = null }) => status === "fulfilled" && value !== null)
-								.map(({ value = null }, index) => {
-									const { data = null } = value;
+					return config;
+				}),
+			(res) => Promise.resolve(res),
+			(err) => Promise.reject(err)
+		);
 
-									values[index] = data;
+		RequestAxiosInstance.interceptors.response.use(
+			(res) => {
+				// Send log message when endpoint request is successful
+				console.info(`[${convertStringToUppercase(method)}] ${updatedUrl} - ${nodeName} - (${res.statusText})`);
 
-									return values[index];
-								});
-						}
+				// Decrement pending requests
+				this.pending_requests = Math.max(0, this.pending_requests - 1);
 
-						return Promise.resolve(values);
-					})
-					.catch((err) => Promise.reject(err));
-			} else if (values && Object.prototype.toString.call(values) === "[object Object]" && Object.keys(values)?.length > 0) {
-				let promises = [];
+				return res;
+			},
+			async (err) => {
+				if (err.response) {
+					// Send log message when error response is received
+					console.error(`[${convertStringToUppercase(method)}] ${updatedUrl} - ${nodeName} - [ERROR] (${err.message})`);
 
-				if (values && Object.prototype.hasOwnProperty.call(values, "id") && Object.keys(values)?.length > 0) {
-					promises.push(request.run("get", CONTENT_ENDPOINT + values.id + "?expand=*", body));
+					// Death by rate limit is self enforced
+					await this.request(url, method, body, headers);
+				} else if (err.request) {
+					// Send log message when error request is received
+					console.error(`[${convertStringToUppercase(method)}] ${updatedUrl} - ${nodeName} - [ERROR] (${err.message})`);
+
+					// Death by rate limit is self enforced
+					await this.request(url, method, body, headers);
 				} else {
-					throw new Error("Expanded `" + label + "` is missing `id` key");
+					// Send log message when error is thrown
+					console.error(`[${convertStringToUppercase(method)}] ${updatedUrl} - ${nodeName} - [ERROR] (${err.message})`);
 				}
 
-				await Promise.allSettled(promises)
-					.then((res) => {
-						if (res && Array.isArray(res) && res?.length > 0) {
-							res
-								.filter(({ status = null, value = null }) => status === "fulfilled" && value !== null)
-								.map(({ value = null }) => {
-									const { data = null } = value;
+				// Decrement pending requests
+				this.pending_requests = Math.max(0, this.pending_requests - 1);
 
-									values = data;
-
-									return values;
-								});
-						}
-
-						return Promise.resolve(values);
-					})
-					.catch((err) => Promise.reject(err));
-			} else {
-				this.log.error("Current `" + label + "` is not an array, object or null. Skipping...");
-			}
-
-			return values;
-		};
-
-		// Handle content blocks
-		const handleContentBlocks = async (blocks) => {
-			const expandedBlocks = await Promise.allSettled(
-				blocks.map(async (block) => {
-					const temp = Object.assign({}, block);
-
-					const dynamicStylesPromise =
-						temp.contentLink?.expanded?.dynamicStyles && Array.isArray(temp.contentLink?.expanded?.dynamicStyles) && temp.contentLink?.expanded?.dynamicStyles?.length > 0
-							? await handleExpandedKeyValues(temp.contentLink?.expanded?.dynamicStyles, "dynamicStyles")
-							: null;
-					const itemsPromise =
-						temp.contentLink?.expanded?.items && Array.isArray(temp.contentLink?.expanded?.items) && temp.contentLink?.expanded?.items?.length > 0 ? await handleExpandedKeyValues(temp.contentLink?.expanded?.items, "items") : null;
-					const imagesPromise =
-						temp.contentLink?.expanded?.images && Array.isArray(temp.contentLink?.expanded?.images) && temp.contentLink?.expanded?.images?.length > 0 ? await handleExpandedKeyValues(temp.contentLink.expanded.images, "images") : null;
-					const formPromise =
-						temp.contentLink?.expanded?.form && Array.isArray(temp.contentLink?.expanded?.form) && temp.contentLink?.expanded?.form?.length > 0 ? await handleExpandedKeyValues(temp.contentLink?.expanded?.form, "form") : null;
-
-					const expandedKeyValuesPromises = [dynamicStylesPromise, itemsPromise, imagesPromise, formPromise];
-
-					await Promise.allSettled(expandedKeyValuesPromises)
-						.then((res) => {
-							if (res && Array.isArray(res) && res?.length > 0) {
-								res
-									.filter(({ status = null, value = null }) => status === "fulfilled" && value !== null)
-									.map(({ value = null }, index) => {
-										switch (index) {
-											case 0:
-												delete temp.contentLink.expanded.dynamicStyles;
-												temp.contentLink.expanded.dynamicStyles = value;
-												break;
-											case 1:
-												delete temp.contentLink.expanded.items;
-												temp.contentLink.expanded.items = value;
-												break;
-											case 2:
-												delete temp.contentLink.expanded.images;
-												temp.contentLink.expanded.images = value;
-												break;
-											case 3:
-												delete temp.contentLink.expanded.form;
-												temp.contentLink.expanded.form = value;
-												break;
-											default:
-												break;
-										}
-
-										return temp;
-									});
-
-								return Promise.resolve(temp);
-							}
-						})
-						.catch((err) => Promise.reject(err));
-
-					return temp;
-				})
-			)
-				.then((res) => {
-					if (res && Array.isArray(res) && res?.length > 0) {
-						return Promise.resolve(res.filter(({ status = null, value = null }) => status === "fulfilled" && value !== null).map(({ value = null }) => value));
-					}
-				})
-				.catch((err) => Promise.reject(err));
-
-			return expandedBlocks;
-		};
-
-		// Handle expanded data
-		if (data && Array.isArray(data) && data?.length > 0) {
-			const expandedData = await Promise.allSettled(
-				data.map(async (item) => {
-					const temp = Object.assign({}, item);
-
-					const { contentBlocks = null, contentBlocksTop = null, contentBlocksBottom = null } = temp;
-
-					if (contentBlocks && Array.isArray(contentBlocks) && contentBlocks?.length > 0) {
-						let expandedContentBlocks = await handleContentBlocks(contentBlocks);
-
-						temp.contentBlocks = expandedContentBlocks;
-					}
-
-					if (contentBlocksTop && Array.isArray(contentBlocksTop) && contentBlocksTop?.length > 0) {
-						let expandedContentBlocksTop = await handleContentBlocks(contentBlocksTop);
-
-						temp.contentBlocksTop = expandedContentBlocksTop;
-					}
-
-					if (contentBlocksBottom && Array.isArray(contentBlocksBottom) && contentBlocksBottom?.length > 0) {
-						let expandedContentBlocksBottom = await handleContentBlocks(contentBlocksBottom);
-
-						temp.contentBlocksBottom = expandedContentBlocksBottom;
-					}
-
-					return temp;
-				})
-			)
-				.then((res) => {
-					if (res && Array.isArray(res) && res?.length > 0) {
-						return Promise.resolve(res.filter(({ status, value }) => status === "fulfilled" && value !== null).map(({ value }) => value));
-					}
-				})
-				.catch((err) => Promise.reject(err));
-
-			return expandedData;
-		} else if (data && Object.prototype.toString.call(data) === "[object Object]" && Object.keys(data)?.length > 0) {
-			try {
-				const expandedDataPromise = new Promise((resolve, reject) => {
-					const temp = Object.assign({}, data);
-
-					const { contentBlocks = null, contentBlocksTop = null, contentBlocksBottom = null } = temp;
-
-					if (contentBlocks && Array.isArray(contentBlocks) && contentBlocks?.length > 0) {
-						let expandedContentBlocks = handleContentBlocks(contentBlocks);
-
-						temp.contentBlocks = expandedContentBlocks;
-					}
-
-					if (contentBlocksTop && Array.isArray(contentBlocksTop) && contentBlocksTop?.length > 0) {
-						let expandedContentBlocksTop = handleContentBlocks(contentBlocksTop);
-
-						temp.contentBlocksTop = expandedContentBlocksTop;
-					}
-
-					if (contentBlocksBottom && Array.isArray(contentBlocksBottom) && contentBlocksBottom?.length > 0) {
-						let expandedContentBlocksBottom = handleContentBlocks(contentBlocksBottom);
-
-						temp.contentBlocksBottom = expandedContentBlocksBottom;
-					}
-
-					return temp ? resolve(temp) : reject(temp);
-				});
-
-				return expandedDataPromise;
-			} catch (err) {
 				return Promise.reject(err);
 			}
-		} else return data;
-	}
+		);
 
-	// Handle `GET` request
-	async get(path, headers = {}) {
-		sleep(this.request_throttle_interval);
+		const updatedMethod = convertStringToLowercase(method);
 
-		const response = await this.request("get", path, headers);
-		return response;
-	}
-
-	// Handle `POST` request
-	async post(path, body, headers = {}) {
-		sleep(this.request_throttle_interval);
-
-		const response = await this.request("post", path, body, headers);
-		return response;
+		switch (updatedMethod) {
+			case "get": {
+				const response = await RequestAxiosInstance.get(url, body)
+					.then((res) => Promise.resolve(res?.data || null))
+					.catch((err) => Promise.reject(err));
+				return response;
+			}
+			case "post": {
+				const response = await RequestAxiosInstance.post(url, body)
+					.then((res) => Promise.resolve(res?.data || null))
+					.catch((err) => Promise.reject(err));
+				return response;
+			}
+			default:
+				throw new Error(`The ${updatedMethod} method is currently not supported.`);
+		}
 	}
 }
 
